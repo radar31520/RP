864e27b0136ffa947fbe8698cf8f92e4702cc888
Radar31520 <godsprop7@hotmail.com>
2012-10-28 16:09:47 -0400

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RpShop::Application.config.secret_token = '76232bd53ff0bf6cf6103f9abac639d16aafc1e059f0a436ef504b4d2445ee06a9debd86ad33283ce5a12eb1ef9645ccaf358bbcf2633242bd1855e0715b00e5'
